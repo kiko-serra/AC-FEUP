2fc006d97e1563db4d489a9a49f3a90db47c7dd3
Francisco Serra <84729439+kiko-serra@users.noreply.github.com>
2022-03-08 10:24:38 +0000

    changed .c file

--- FEUP_LEIC_AC2021.c
+++ FEUP_LEIC_AC2021.c
@@ -8,6 +8,7 @@
  ============================================================================
  */
 
+/* tutorial
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,6 +19,20 @@ int main(void) {
 	int b = 10;
 	int ca = 0;
 	ca = SOMA(a,b);
-	printf("A soma é %d\n", ca);
+	printf("A soma Ã© %d\n", ca);
+	return EXIT_SUCCESS;
+}
+*/
+//ex1
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int SOMA_V(int *a , int n);
+int main(void){
+	int dim = 5;
+	int v [] = {3 , -1 , 8 , 0 , -3}; // Para testar : 32 bits
+	int res ;
+	res = SOMA_V(v , dim ) ;
+	printf("Soma dos elementos = %d\n", res ) ;
 	return EXIT_SUCCESS;
 }
