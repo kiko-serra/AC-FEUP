e457cc1d5788f34c542787a167f7fb0a0c22a5c2
Francisco Serra <up202007723@up.pt>
2022-05-11 17:08:12 +0100

    Changed POLI resolution

--- FICHA2/POLI.s
+++ FICHA2/POLI.s
@@ -2,7 +2,9 @@
 .global POLI
 .type POLI ,"function"
 
-POLI:   MOV X11, X30
+POLI:   stp x29, x30, [SP, -32]! // precisamos de guardar x29 (FP) e x30 (LR) para repor no fim
+	str x20, [SP, 16] // guardar x20 pois é registo protegido e é usado no corpo da função POLI
+	mov x29, SP // atualizar FP
 		mov X10, X0 // <1>
 		bl QUAD
 		mov X20, X0
@@ -10,7 +12,8 @@ POLI:   MOV X11, X30
 		mul X10, X10, X0
 		add X0, X20, X10
 		add X0, X0, 1
-		MOV X30, X11
+		ldr x20, [SP, 16] // repor x20 
+	ldp x29, x30, [SP], 32 // POP da frame
 FIM:	ret // <2>
 QUAD:
 		mul X0, X0, X0
